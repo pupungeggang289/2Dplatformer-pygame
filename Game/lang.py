ad0e24f110d93fcfb253bda1da307e0e88e5d893
pupungeggang289 <kyongkyong2892@gmail.com>
2023-02-23 23:30:56 +0900

    0.0.0

--- Game/lang.py
+++ Game/lang.py
@@ -1,9 +1,13 @@
 lang = {
     'en' : {
-        'game_title' : '2D Platformer Sample'
+        'game_title' : '2D Platformer Sample',
+        'start_text' : 'Start',
+        'lang' : 'English',
     },
 
     'ko' : {
-        'game_title' : '2D 플랫포머 샘플'
+        'game_title' : '2D 플랫포머 샘플',
+        'start_text' : '시작',
+        'lang' : '한국어'
     },
 }
--- Game/title.py
+++ Game/title.py
@@ -14,7 +14,18 @@ def loop():
 def display():
     var.screen.fill(const.Color.white)
     var.screen.blit(const.Font.title.render(lang.lang[var.lang]['game_title'], True, const.Color.black), UI.title_text)
+
+    pygame.draw.rect(var.screen, const.Color.black, UI.Title.button_start, 2)
+    var.screen.blit(const.Font.title.render(lang.lang[var.lang]['start_text'], True, const.Color.black), UI.Title.text_start)
+    pygame.draw.rect(var.screen, const.Color.black, UI.Title.button_lang, 2)
+    var.screen.blit(const.Font.title.render(lang.lang[var.lang]['lang'], True, const.Color.black), UI.Title.text_lang)
     pygame.display.flip()
 
 def mouse_up(x, y, button):
-    pass
+    if button == 1:
+        if var.state == '':
+            if physics.point_inside_rect_array(x, y, UI.Title.button_lang):
+                if var.lang == 'en':
+                    var.lang = 'ko'
+                elif var.lang == 'ko':
+                    var.lang = 'en'
